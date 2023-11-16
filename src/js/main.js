cd42d0afd072cf7dbe563c24ab5b75c6dd8f64ef
dimitricaloz <dimitri.caloz@studentfr.ch>
2023-11-16 17:01:06 +0100

    fin de journée 16.11.2023

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,57 @@
 import { gsap } from "gsap";
 
-gsap.to(".slider-track", {
-  x: "-75%",
-});
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-document.addEventListener("DOMContentLoaded", function () {
-  var buttons = [
-    document.getElementById("button-start"),
-    document.getElementById("button-one"),
-    document.getElementById("button-two"),
-    document.getElementById("button-three"),
-    document.getElementById("button-four"),
-    document.getElementById("button-five"),
-    document.getElementById("button-six"),
-    document.getElementById("button-seven"),
-    document.getElementById("button-heigt"),
-    document.getElementById("button-nine"),
-    document.getElementById("button-ten"),
-  ];
+gsap.registerPlugin(ScrollToPlugin);
+// document.addEventListener("DOMContentLoaded", function () {
+//   var buttons = [
+//     document.getElementById("button-start"),
+//     document.getElementById("button-one"),
+//     document.getElementById("button-two"),
+//     document.getElementById("button-three"),
+//     document.getElementById("button-four"),
+//     document.getElementById("button-five"),
+//     document.getElementById("button-six"),
+//     document.getElementById("button-seven"),
+//     document.getElementById("button-heigt"),
+//     document.getElementById("button-nine"),
+//     document.getElementById("button-ten"),
+//   ];
 
-  var sections = [
-    document.getElementById("page-one"),
-    document.getElementById("page-two"),
-    document.getElementById("page-three"),
-    document.getElementById("page-four"),
-    document.getElementById("page-five"),
-    document.getElementById("page-six"),
-    document.getElementById("page-seven"),
-    document.getElementById("page-height"),
-    document.getElementById("page-nine"),
-    document.getElementById("final-page"),
-  ];
+//   var sections = [
+//     document.getElementById("page-one"),
+//     document.getElementById("page-two"),
+//     document.getElementById("page-three"),
+//     document.getElementById("page-four"),
+//     document.getElementById("page-five"),
+//     document.getElementById("page-six"),
+//     document.getElementById("page-seven"),
+//     document.getElementById("page-height"),
+//     document.getElementById("page-nine"),
+//     document.getElementById("final-page"),
+//   ];
 
-  buttons.forEach(function (button, index) {
+//   buttons.forEach(function (button, index) {
+//     button.addEventListener("click", function () {
+//       sections[index].classList.remove("hidden");
+//     });
+//   });
+// });
+
+var buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function (button, index) {
+  let section = button.getAttribute("data-section");
+  if (section) {
     button.addEventListener("click", function () {
-      sections[index].classList.remove("hidden");
+      document.querySelector(section).classList.remove("hidden");
+      gsap.to("html", {
+        duration: 1,
+        scrollTo: {
+          x: section,
+        },
+        ease: "power2.inOut",
+      });
     });
-  });
+  }
 });
