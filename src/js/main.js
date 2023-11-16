e68a0b03cc75fb84ce88e3d39c9c6d3fdc537dd9
dimitricaloz <dimitri.caloz@studentfr.ch>
2023-11-16 12:20:49 +0100

    pause midi 16.11.2023

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,37 @@ gsap.to(".slider-track", {
   x: "-75%",
 });
 
-// gsap.to(".slider-track", {
-//   scrollTrigger: {
-//     trigger: ".slider-mask",
-//     start: "top top",
-//     end: "+=300%",
-//     pin: true,
-//     scrub: true,
-//     markers: true,
-//   },
-//   x: amplitudeX + "%",
-//   ease: "sine.inOut",
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = [
+    document.getElementById("button-start"),
+    document.getElementById("button-one"),
+    document.getElementById("button-two"),
+    document.getElementById("button-three"),
+    document.getElementById("button-four"),
+    document.getElementById("button-five"),
+    document.getElementById("button-six"),
+    document.getElementById("button-seven"),
+    document.getElementById("button-heigt"),
+    document.getElementById("button-nine"),
+    document.getElementById("button-ten"),
+  ];
+
+  var sections = [
+    document.getElementById("page-one"),
+    document.getElementById("page-two"),
+    document.getElementById("page-three"),
+    document.getElementById("page-four"),
+    document.getElementById("page-five"),
+    document.getElementById("page-six"),
+    document.getElementById("page-seven"),
+    document.getElementById("page-height"),
+    document.getElementById("page-nine"),
+    document.getElementById("final-page"),
+  ];
+
+  buttons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
+      sections[index].classList.remove("hidden");
+    });
+  });
+});
